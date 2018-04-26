205cfab64279ae5d6808b8ce27d39703f23d36c1
zhangmiaoping23 <645415776@qq.com>
2018-04-26 10:31:06 +0800

    1.增加LOG输出

--- Hardware/app/src/main/java/com/example/hardware/MainActivity.java
+++ Hardware/app/src/main/java/com/example/hardware/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.hardware;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.example.hardware.Util.LogUtils;
+
 public class MainActivity extends Activity {
 
     @Override
@@ -10,39 +12,94 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         getBuildHardware();
         String imei = HardwareUtils.getImei(this);
+        LogUtils.i(String.format("imei=%s",imei));
         String userAgent = HardwareUtils.getSystemUserAgent();
+        LogUtils.i(String.format("userAgent=%s",userAgent));
         setContentView(R.layout.activity_main);
     }
 
     public void getBuildHardware(){
         String buildBoard = HardwareUtils.getBuildBoard();
+        LogUtils.i(String.format("buildBoard=%s",buildBoard));
+
         String buildBootloader = HardwareUtils.getBuildBootLoader();
+        LogUtils.i(String.format("buildBootloader=%s",buildBootloader));
+
         String buildBrand = HardwareUtils.getBuildBrand();
+        LogUtils.i(String.format("buildBrand=%s",buildBrand));
+
         String buildCpuApi = HardwareUtils.getbuildCpuApi();
+        LogUtils.i(String.format("buildCpuApi=%s",buildCpuApi));
+
         String buildCpuApi2 = HardwareUtils.getbuildCpuApi2();
+        LogUtils.i(String.format("buildCpuApi2=%s",buildCpuApi2));
+
         String buildDevice = HardwareUtils.getBuildDevice();
+        LogUtils.i(String.format("buildDevice=%s",buildDevice));
+
         String buildDisplay = HardwareUtils.getBuildDisplay();
+        LogUtils.i(String.format("buildDisplay=%s",buildDisplay));
+
         String buildFingerPrint = HardwareUtils.getBuildFingerPrint();
+        LogUtils.i(String.format("buildFingerPrint=%s",buildFingerPrint));
+
         String buildRadioVersion = HardwareUtils.getBuildRadioVersion();
+        LogUtils.i(String.format("buildRadioVersion=%s",buildRadioVersion));
+
         String buildSerial = HardwareUtils.getBuildSerial();
+        LogUtils.i(String.format("buildSerial=%s",buildSerial));
+
         String buildHardware = HardwareUtils.getBuildHardware();
+        LogUtils.i(String.format("buildHardware=%s",buildHardware));
+
         String buildHost = HardwareUtils.getBuildHost();
+        LogUtils.i(String.format("buildHost=%s",buildHost));
+
         String buildId = HardwareUtils.getBuildId();
+        LogUtils.i(String.format("buildId=%s",buildId));
+
         String buildManufacturer = HardwareUtils.getBuildManufacturer();
+        LogUtils.i(String.format("buildManufacturer=%s",buildManufacturer));
+
         String buildModel = HardwareUtils.getBuildModel();
+        LogUtils.i(String.format("buildModel=%s",buildModel));
+
         String buildProduct = HardwareUtils.getBuildProduct();
+        LogUtils.i(String.format("buildProduct=%s",buildProduct));
+
         String buildTags = HardwareUtils.getBuildTags();
+        LogUtils.i(String.format("buildTags=%s",buildTags));
+
         String buildTime = HardwareUtils.getBuildTime();
+        LogUtils.i(String.format("buildTime=%s",buildTime));
+
         String buildType = HardwareUtils.getBuildType();
+        LogUtils.i(String.format("buildType=%s",buildType));
+
         String buildUser = HardwareUtils.getBuildUser();
+        LogUtils.i(String.format("buildUser=%s",buildUser));
+
         String buildVersionBaseOS = HardwareUtils.getBuildVersionBaseOS();
+        LogUtils.i(String.format("buildVersionBaseOS=%s",buildVersionBaseOS));
+
         String buildVersionCodeName = HardwareUtils.getBuildVersionCodeName();
+        LogUtils.i(String.format("buildVersionCodeName=%s",buildVersionCodeName));
+
         String buildVersionIncremental = HardwareUtils.getBuildVersionIncremental();
+        LogUtils.i(String.format("buildVersionIncremental=%s",buildVersionIncremental));
+
         String buildVersionRelease = HardwareUtils.getBuildVersionRelease();
+        LogUtils.i(String.format("buildVersionRelease=%s",buildVersionRelease));
+
         String buildVersionSecurityPatch = HardwareUtils.getBuildVersionSecurityPatch();
+        LogUtils.i(String.format("buildVersionSecurityPatch=%s",buildVersionSecurityPatch));
+
         String buildVersionSDK = HardwareUtils.getBuildVersionSDK();
+        LogUtils.i(String.format("buildVersionSDK=%s",buildVersionSDK));
 
         String firmwareVersion = HardwareUtils.getFirmwareVersion();
+        LogUtils.i(String.format("firmwareVersion=%s",firmwareVersion));
+
     }
 
 }
---
+++ Hardware/app/src/main/java/com/example/hardware/Util/LogUtils.java
@@ -0,0 +1,27 @@
+package com.example.hardware.Util;
+
+import android.util.Log;
+
+/**
+ * Created by zhangmp on 2018/4/26.
+ */
+
+public class LogUtils {
+    public static String defaultTag = "AndroidHardware";
+
+    public static void i(String msg) {  //信息太长,分段打印
+       i(defaultTag,msg);
+    }
+    public static void i(String tag, String msg) {  //信息太长,分段打印
+        //因为String的length是字符数量不是字节数量所以为了防止中文字符过多，
+        //  把4*1024的MAX字节打印长度改为2001字符数
+        int max_str_length = 2001 - tag.length();
+        //大于4000时
+        while (msg.length() > max_str_length) {
+            Log.i(tag, msg.substring(0, max_str_length));
+            msg = msg.substring(max_str_length);
+        }
+        //剩余部分
+        Log.i(tag, msg);
+    }
+}
