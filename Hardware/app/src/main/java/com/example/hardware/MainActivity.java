cc4e0fe5397efff97fbdd7af398d2cfc16c84dcf
zhangmiaoping23 <645415776@qq.com>
2018-05-15 17:22:52 +0800

    oppo属性InsVer

--- Hardware/app/src/main/java/com/example/hardware/MainActivity.java
+++ Hardware/app/src/main/java/com/example/hardware/MainActivity.java
@@ -25,7 +25,6 @@ public class MainActivity extends Activity {
         LogUtils.i(String.format("userAgent=%s",userAgent));
 
         OppoHardwareUtils.getHardwares(this);
-
         setContentView(R.layout.activity_main);
     }
     /*
--- Hardware/app/src/main/java/com/example/hardware/Util/OppoHardwareUtils.java
+++ Hardware/app/src/main/java/com/example/hardware/Util/OppoHardwareUtils.java
@@ -1,9 +1,13 @@
 package com.example.hardware.Util;
 
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.text.TextUtils;
 
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Method;
+import java.net.URLEncoder;
 
 public class OppoHardwareUtils {
     public static void getHardwares(Context context){
@@ -19,6 +23,8 @@ public class OppoHardwareUtils {
 
             String colorImei = OppoHardwareUtils.reflectColorImei(context);
             LogUtils.i(String.format("oppo colorImei(higher than imei)=%s",colorImei));
+            String insVer = getInsVer(context);
+            LogUtils.i(String.format("oppo insVer=%s",colorImei));
         }
     }
 
@@ -60,5 +66,137 @@ public class OppoHardwareUtils {
             exception.printStackTrace();
         }
         return romName;
+    }
+
+    public static String getInsVer(Context argContext) {
+        String ret = "-1";
+        int failResult = -1;
+        if(isPackageInstantPlatformInstalled(argContext, "com.nearme.instant.platform")) {
+            int platformVersion = getPlatformVersion(argContext);                   //1000
+            int platformApiLevel = getPlatformApiLevel(argContext);                 //10100
+            int platformBizVersion = getPlatformBizVersion(argContext);             //1
+            if(failResult != platformVersion && failResult != platformApiLevel && failResult != platformBizVersion) {
+                StringBuilder stringBuilder = new StringBuilder().append(platformApiLevel).append("/").append(platformVersion).append("/").append(platformBizVersion);
+                try {
+                    ret = URLEncoder.encode(stringBuilder.toString(), "UTF-8");
+                }
+                catch(UnsupportedEncodingException exception) {
+
+                }
+
+                return ret;
+            }
+
+            ret = "-1";
+        }
+        else {
+            ret = "-1";
+        }
+
+        return ret;
+    }
+
+    private static boolean isPackageInstantPlatformInstalled(Context argContext, String packageName) {
+        boolean ret = false;
+        PackageManager packageManager = argContext.getPackageManager();
+        try {
+            ApplicationInfo applicationInfo = packageManager.getApplicationInfo("com.nearme.instant.platform", packageManager.GET_META_DATA);
+            if(applicationInfo == null) {
+                return ret;
+            }
+
+            if(!applicationInfo.packageName.equals(packageName)) {
+                return ret;
+            }
+        }
+        catch(Exception exception) {
+            return ret;
+        }
+
+        return true;
+    }
+
+    private static int getPlatformVersion(Context argContext) {
+        int ret = -1;
+        PackageManager packageManager = argContext.getPackageManager();
+        try {
+            ApplicationInfo applicationInfo = packageManager.getApplicationInfo("com.nearme.instant.platform", packageManager.GET_META_DATA);
+            if(applicationInfo == null) {
+                return ret;
+            }
+
+            Object objectPlatformVersion = applicationInfo.metaData.get("platformVersion");
+            if(objectPlatformVersion == null) {
+                return ret;
+            }
+
+            if(!(objectPlatformVersion instanceof Integer)) {
+                return ret;
+            }
+
+            int platformVersion = ((Integer)objectPlatformVersion).intValue();
+            return platformVersion;
+        }
+        catch(Exception exception) {
+            exception.printStackTrace();
+        }
+
+        return ret;
+    }
+
+    private static int getPlatformApiLevel(Context argContext) {
+        int ret = -1;
+        PackageManager packageManager = argContext.getPackageManager();
+        try {
+            ApplicationInfo applicationInfo = packageManager.getApplicationInfo("com.nearme.instant.platform", packageManager.GET_META_DATA);
+            if(applicationInfo == null) {
+                return ret;
+            }
+
+            Object objectApiLevel = applicationInfo.metaData.get("api_level");
+            if(objectApiLevel == null) {
+                return ret;
+            }
+
+            if(!(objectApiLevel instanceof Integer)) {
+                return ret;
+            }
+
+            int apiLevel = ((Integer)objectApiLevel).intValue();
+            return apiLevel;
+        }
+        catch(Exception exception) {
+            exception.printStackTrace();
+        }
+
+        return ret;
+    }
+
+    private static int getPlatformBizVersion(Context argContext) {
+        int ret = -1;
+        PackageManager packageManager = argContext.getPackageManager();
+        try {
+            ApplicationInfo applicationInfo = packageManager.getApplicationInfo("com.nearme.instant.platform", 128);
+            if(applicationInfo == null) {
+                return ret;
+            }
+
+            Object objectBizVersion = applicationInfo.metaData.get("biz_version");
+            if(objectBizVersion == null) {
+                return ret;
+            }
+
+            if(!(objectBizVersion instanceof Integer)) {
+                return ret;
+            }
+
+            int bizVersion = ((Integer)objectBizVersion).intValue();
+            return bizVersion;
+        }
+        catch(Exception exception) {
+            exception.printStackTrace();
+        }
+
+        return ret;
     }
 }
